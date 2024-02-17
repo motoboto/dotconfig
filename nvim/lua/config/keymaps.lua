ed4d20ef07ad2193673be548456830a9fd6f54c2
Lenar Kamaev <motolenar@gmail.com>
2024-02-18 01:23:11 +0300

    add nvim config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<leader>o", function()
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd.wincmd("p")
+  else
+    vim.cmd.Neotree("focus")
+  end
+end, { desc = "Toggle Explorer Focus" })
+-- vim.keymap.set("n", "te", "tabedit", opts)
+vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", opts)
+vim.keymap.set("n", "<s-tab>", "<cmd>bprevious<cr>", opts)
